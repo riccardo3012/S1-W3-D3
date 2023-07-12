a71abadc10815a0cbe30115c6d6a5e3316814921
riccardo ronca <ronca.rcr@gmail.com>
2023-07-12 17:02:52 +0200

    S1-W3-D3

---
+++ index.js
@@ -0,0 +1,17 @@
+function addb() {
+  let taskName = document.getElementById("nameForm").value;
+  let list = document.getElementById("list");
+  let newDo = document.createElement("li");
+  newDo.textContent = taskName;
+  let cancButton = document.createElement("button");
+
+  cancButton.textContent = "DELETE";
+  cancButton.classList.add("delete-button");
+  newDo.appendChild(cancButton);
+
+  cancButton.addEventListener("click", function () {
+    this.parentNode.remove();
+  });
+
+  list.appendChild(newDo);
+}
