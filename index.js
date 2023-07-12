7add02e3a9cf857acf3bd97fe1dd2e3ed5ae90cb
riccardo ronca <ronca.rcr@gmail.com>
2023-07-12 17:07:44 +0200

    S1-W3-D3

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 function addb() {
   let taskName = document.getElementById("nameForm").value;
+
   let list = document.getElementById("list");
+
   let newDo = document.createElement("li");
+
   newDo.textContent = taskName;
+
   let cancButton = document.createElement("button");
 
   cancButton.textContent = "DELETE";
+
   cancButton.classList.add("delete-button");
+
   newDo.appendChild(cancButton);
 
   cancButton.addEventListener("click", function () {
